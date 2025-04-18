07853c4e26cedcf245be0a1a0ec9d90311ae1fe5
ridzkyy <ridzky@lacak.io>
2025-04-18 20:16:33 +0700

    Fix typo in Hero component description and remove temporary swap files

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
           Anggi Lestari Phaja
         </SectionTitle>
         <SectionText>
-          I'm a filmaker and photographer based in Indonesia. I specialize in creating stunning visuals that tell compelling stories. My work spans various genres, including documentaries, commercials, and narrative films. I am passionate about capturing the essence of moments and bringing ideas to life through the lens.
+          I'm a filmmaker and photographer based in Indonesia. I specialize in creating stunning visuals that tell compelling stories. My work spans various genres, including documentaries, commercials, and narrative films. I am passionate about capturing the essence of moments and bringing ideas to life through the lens.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
