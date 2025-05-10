6d4a2e5cc58bdc8fe46a4928e203a534617e16cc
Ridzky <Ridzky@lacak.io>
2025-05-11 01:16:49 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
+// filepath: d:\code\joks\joks\next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Required for GitHub Pages compatibility
-  basePath: '/web_joks',
-  assetPrefix: '/web_joks/',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
